dba0c21218eb82898ff0629b66c594c614c43871
jxs <you@example.com>
2020-06-11 14:02:29 +0800

    上传

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+	title: '蒋鲜生の前端学习笔记',
+	description: '这是一个总结前端知识的网站鸭！',
+	  themeConfig: {
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Guide', link: '/guide/' },
+			{ text: 'External', link: 'https://google.com' },
+		  ],
+		  sidebar: [
+			{
+			  title: '前言',
+			  collapsable: false,
+			  children: [
+				'/',
+			  ]
+			},
+			{
+				title: 'Vue',
+				children: ['./react/react1','./react/react2' ]
+			},
+			{
+				title: 'Js',
+				children: ['./js/string对象']
+			  },
+			{
+				title: 'React',
+				children: ['./react/react1','./react/react2' ]
+			},
+			{
+				title: '面试题',
+				children: ['./面试题/跨域的几种方式']
+			}
+		  ]
+	  }
+  }
+
+
+ 
---
+++ readme.md
@@ -0,0 +1,8 @@
+# 关于项目的启动
+
+yarn docs:dev # 或者：npm run docs:dev   运行
+
+yarn docs:build # 或者：npm run docs:build  打包
+
+
+页面配置在docs -> .vuepress -> config.js
